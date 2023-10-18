81dc3d201a5ce62feac58c6c3238b535468217e2
Morton <>
2023-10-18 10:49:15 +0800

    fix type error

--- public/assets/svg/index.ts
+++ public/assets/svg/index.ts
@@ -2,6 +2,6 @@ import HiCreditCard from './hiCreditCard.svg';
 import PlaceIcon from './placeIcon.svg';
 import ChooseMarketIcon from './chooseMarketIcon.svg';
 import EarnIcon from './earnIcon.svg';
-import CommunityIcon from './CommunityIcon.svg';
+import CommunityIcon from './communityIcon.svg';
 
 export { HiCreditCard, PlaceIcon, ChooseMarketIcon, EarnIcon, CommunityIcon };
