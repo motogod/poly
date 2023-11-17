3b55d2d648b564d5aebb9747bee1a9829664266e
Morton <>
2023-11-17 18:13:43 +0800

    add comment

--- src/hooks/useLoginModal/useLoginModal.tsx
+++ src/hooks/useLoginModal/useLoginModal.tsx
@@ -102,10 +102,11 @@ function useLoginModal() {
 					size={isDesktop ? 'lg' : 'full'}
 					isOpen={isOpen}
 					onClose={() => {
-						setPopupGoogle(false);
-						disconnect();
-						resetSignIn();
-						onClose();
+						// 若使用者關閉視窗，重置相關設置
+						setPopupGoogle(false); // 恢復 google 彈窗出現與否的值
+						disconnect(); // 斷開錢包連接
+						resetSignIn(); // 斷開當下簽名請求
+						onClose(); // 關閉視窗
 					}}
 				>
 					<ModalOverlay zIndex={zIndexLoginModal} />
