4b768e873db98b76b667a113c7f69f8acc9276dc
Morton <>
2024-01-08 18:15:17 +0800

    add buy sell ui logic

--- src/components/MraketsDetail/BuyOrSellContent/BuyOrSellContent.tsx
+++ src/components/MraketsDetail/BuyOrSellContent/BuyOrSellContent.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import {
 	Stack,
 	Card,
 	Heading,
 	Button,
+	NumberInput,
 	Input,
 	useNumberInput,
 	HStack,
@@ -11,6 +13,8 @@ import {
 	Select,
 	Tag,
 	TagLabel,
+	Collapse,
+	Text,
 } from '@chakra-ui/react';
 import { Icon } from '@chakra-ui/react';
 import { BiWalletAlt } from 'react-icons/bi';
@@ -26,10 +30,13 @@ import { useLoginModal } from '@/hooks';
 import BuyOrSellButton from '../Buttons/BuyOrSellButton';
 import YesOrNoButton from '../Buttons/YesOrNoButton';
 import { zIndexMinimum } from '@/utils/zIndex';
+import { log } from 'console';
 
 type SelectedType = 'market' | 'limit';
 
 function BuyOrSellContent() {
+	const router = useRouter();
+
 	const [isBuy, setIsBuy] = useState(true);
 	const [isYes, setIsYes] = useState(true);
 	const [selected, setSelected] = useState<SelectedType>('market');
@@ -41,6 +48,7 @@ function BuyOrSellContent() {
 	const { isUserClickYesOrNo, marketDetailData } = useSelector(
 		(state: RootState) => state.homeReducer
 	);
+	console.log('');
 	// 這邊的 hook 會導致觸發 disconnect 如果要導入 待處理
 	const {
 		ModalDom,
@@ -49,30 +57,48 @@ function BuyOrSellContent() {
 		onClose: modalOnClose,
 	} = useLoginModal();
 
+	// Limit Price
+	const [limiInputValue, setLimitInputValue] = useState(0);
+
 	const {
 		getInputProps: getLimitInputProps,
 		getIncrementButtonProps: getIncLimitBtnProps,
 		getDecrementButtonProps: getDescBtnProps,
 	} = useNumberInput({
-		step: 0.01,
-		defaultValue: 0.0,
-		min: 0.0,
+		step: 0.1,
+		value: limiInputValue,
+		min: 0.01,
 		max: 1.0,
 		precision: 2,
 	});
 
-	// Limit Price
 	const incLimitPrice = getIncLimitBtnProps();
 	const decLimitPrice = getDescBtnProps();
 	const inputLimitPrice = getLimitInputProps();
 
-	console.log('inputLimitPrice', inputLimitPrice.value);
+	// 使用者切換 Yes No 改變 Limit input 的預設值
+	useEffect(() => {
+		if (router.isReady && Object.keys(marketDetailData).length > 0) {
+			if (isUserClickYesOrNo) {
+				setLimitInputValue(marketDetailData.outcome.yes);
+			} else {
+				setLimitInputValue(marketDetailData.outcome.no);
+			}
+		}
+	}, [isUserClickYesOrNo, marketDetailData, router]);
+
+	// Shares
+	const [shareInputValue, setShareInputValue] = useState(0);
+
+	// console.log('shareInputValue =>', shareInputValue);
 
 	const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } = useNumberInput({
 		step: 1,
-		defaultValue: 0.0,
+		// defaultValue: 0.0,
+		value: shareInputValue,
+		//defaultValue: shareInputValue,
 		min: 0,
-		max: 1000,
+		max: hold,
 		precision: 0,
 	});
 
@@ -80,8 +106,6 @@ function BuyOrSellContent() {
 	const incShares = getIncrementButtonProps();
 	const decShares = getDecrementButtonProps();
 	const inputShares = getInputProps();
-
-	console.log('inputShares', inputShares.value);
 
 	const renderConfirmButtonText = () => {
 		if (isAuthenticated) {
@@ -176,19 +200,53 @@ function BuyOrSellContent() {
 							</Heading>
 						</Stack>
 						<HStack mt={'16px'} gap={0} maxW="100%">
-							<Button borderRadius={'6px 0px 0px 6px'} {...decLimitPrice}>
+							<Button
+								onClick={() => {
+									if (limiInputValue > 0) {
+										setLimitInputValue(prev => Number((prev - 0.1).toFixed(2)));
+									}
+								}}
+								borderRadius={'6px 0px 0px 6px'}
+								{...decLimitPrice}
+							>
 								-
 							</Button>
 							<Input
+								type="number"
 								textAlign={'center'}
 								borderRadius={0}
 								border="1px solid #E2E8F0;"
-								{...inputLimitPrice}
+								value={limiInputValue}
+								onChange={e => {
+									const enterValue = Number(e.target.value);
+
+									if (enterValue < 1) {
+										setLimitInputValue(Number(enterValue.toFixed(2)));
+									}
+
+									if (enterValue >= 1) {
+										setLimitInputValue(1);
+									}
+								}}
+								// {...inputLimitPrice}
 							/>
-							<Button borderRadius={'0px 6px 6px 0px'} {...incLimitPrice}>
+							<Button
+								onClick={() => {
+									if (limiInputValue < 1) {
+										setLimitInputValue(prev => Number((prev + 0.1).toFixed(2)));
+									}
+								}}
+								borderRadius={'0px 6px 6px 0px'}
+								{...incLimitPrice}
+							>
 								+
 							</Button>
 						</HStack>
+						<Collapse in={limiInputValue < 0.01 || limiInputValue > 1} animateOpacity>
+							<Text fontSize={'sm'} mt={0} color={'red.500'}>
+								Price should be 0.01 ~ 1.00
+							</Text>
+						</Collapse>
 					</Stack>
 				)}
 				<Stack mt={'24px'}>
@@ -198,24 +256,54 @@ function BuyOrSellContent() {
 						</Heading>
 						<Stack direction={'row'}>
 							<Tag bg={'gray.100'} color={'gray.800'} borderRadius={20} pl={'16px'} pr={'16px'}>
-								<TagLabel>{`Balance: ${hold} USDT`}</TagLabel>
+								<TagLabel>{`Funds: ${hold} USDT`}</TagLabel>
 							</Tag>
-							<Button w={'57px'} h={'28px'} size="xs" bg="gray.600" color="#fff">
+							<Button
+								onClick={() => setShareInputValue(hold)}
+								w={'57px'}
+								h={'28px'}
+								size="xs"
+								bg="gray.600"
+								color="#fff"
+							>
 								Max
 							</Button>
 						</Stack>
 					</Stack>
 					<HStack mt={'16px'} gap={0} maxW="100%">
-						<Button borderRadius={'6px 0px 0px 6px'} {...decShares}>
+						<Button
+							borderRadius={'6px 0px 0px 6px'}
+							onClick={() => {
+								if (shareInputValue > 0) {
+									setShareInputValue(prev => prev - 1);
+								}
+							}}
+							{...decShares}
+						>
 							-
 						</Button>
 						<Input
 							textAlign={'center'}
 							borderRadius={0}
 							border="1px solid #E2E8F0;"
-							{...inputShares}
+							value={shareInputValue}
+							onChange={e => {
+								const enterValue = Number(e.target.value);
+								if (enterValue <= hold) {
+									setShareInputValue(Number(e.target.value));
+								}
+							}}
+							// {...inputShares}
 						/>
-						<Button borderRadius={'0px 6px 6px 0px'} {...incShares}>
+						<Button
+							borderRadius={'0px 6px 6px 0px'}
+							onClick={() => {
+								if (shareInputValue < hold) {
+									setShareInputValue(prev => prev + 1);
+								}
+							}}
+							{...incShares}
+						>
 							+
 						</Button>
 					</HStack>
@@ -224,7 +312,7 @@ function BuyOrSellContent() {
 					onClick={() => {
 						if (isAuthenticated) {
 						} else {
-							// modalOnOpen();
+							modalOnOpen();
 						}
 					}}
 					mt={'24px'}
