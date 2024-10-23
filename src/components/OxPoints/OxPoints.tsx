dbab7ba9d99b43ba2d880c0f7052c777c8cafb6a
Morton <>
2024-10-23 20:45:49 +0800

    adjust ui feeadback

--- src/components/OxPoints/OxPoints.tsx
+++ src/components/OxPoints/OxPoints.tsx
@@ -1,3 +1,314 @@
+// import React, { useEffect } from 'react';
+// import {
+// 	Stack,
+// 	Button,
+// 	Card,
+// 	CardBody,
+// 	Grid,
+// 	Text,
+// 	FormLabel,
+// 	useToast,
+// 	Heading,
+// 	GridItem,
+// 	Tabs,
+// 	TabPanels,
+// 	TabPanel,
+// } from '@chakra-ui/react';
+// import Image from 'next/image';
+// import { useRouter } from 'next/router';
+// import { useMediaQuery } from 'react-responsive';
+// import { useTranslation } from 'next-i18next';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { AppDispatch, RootState } from '@/store';
+// import { headerHeight, paddingMainHorizontal, paddingMainVertical } from '@/utils/screen';
+// import OxImg from '@/../public/assets/svg/icon-ox-points-01.png';
+// import RedeemImg from '@/../public/assets/svg/icon-points-redeem-01.png';
+// import ExchangeImg from '@/../public/assets/svg/image-exchange-01.png';
+// import PointsHistoryImg from '@/../public/assets/svg/icon-points-history-01.png';
+// import OxPointsBannerImg from '@/../public/ox-points-banner-bg.png';
+// import PointsHistoryTable from './PointsHistoryTable';
+// import { getPoints } from '@/store/thunks/fetchPoint';
+
+// // background: linear-gradient(90deg, #edf2f7 44%, #d53f8c 30%);
+// function OxPoints() {
+// 	const router = useRouter();
+
+// 	const { t } = useTranslation();
+
+// 	const toast = useToast();
+
+// 	const isDesktop = useMediaQuery({
+// 		query: '(min-width: 960px)',
+// 	});
+
+// 	const { isAuthenticated } = useSelector((state: RootState) => state.authReducer);
+// 	const { userPointData, meta } = useSelector((state: RootState) => state.pointReducer);
+
+// 	const dispatch = useDispatch<AppDispatch>();
+
+// 	useEffect(() => {
+// 		if (!isAuthenticated && isAuthenticated !== null) {
+// 			router.replace('./');
+// 		}
+// 	}, [router, isAuthenticated]);
+
+// 	useEffect(() => {
+// 		dispatch(getPoints({ page: 1, take: 20 }));
+// 	}, [dispatch]);
+
+// 	const handleTabsChange = (index: number) => {
+// 		if (index === 0) {
+// 		}
+
+// 		if (index === 1) {
+// 		}
+
+// 		if (index === 2) {
+// 		}
+// 	};
+
+// 	return (
+// 		<Stack mt={headerHeight} h={'100vh'}>
+// 			<Stack ml={paddingMainHorizontal} mr={paddingMainHorizontal} mt={paddingMainVertical}>
+// 				<Card position={'relative'} borderRadius="2xl">
+// 					<Stack
+// 						flex={1}
+// 						position="absolute"
+// 						align={'center'}
+// 						w={'100%'}
+// 						padding={34}
+// 						h={{ lg: '400px', md: '400px', sm: '400px' }}
+// 						direction="row"
+// 						borderRadius="2xl"
+// 					>
+// 						<Stack flex={2} minWidth="0">
+// 							<FormLabel
+// 								color={'white'}
+// 								fontSize={{ lg: '36', md: '36', sm: '36' }}
+// 								fontWeight={900}
+// 								whiteSpace="normal" // 允許換行
+// 							>
+// 								OX Points
+// 							</FormLabel>
+// 						</Stack>
+// 					</Stack>
+// 					<Image
+// 						width={0}
+// 						height={0}
+// 						style={{ width: '100%', height: '400px', borderRadius: 10, objectFit: 'cover' }}
+// 						src={OxPointsBannerImg}
+// 						alt="funds_background"
+// 					/>
+// 				</Card>
+// 			</Stack>
+// 			<Grid
+// 				ml={paddingMainHorizontal}
+// 				mr={paddingMainHorizontal}
+// 				templateColumns={{
+// 					lg: 'repeat(2, 1fr)',
+// 					md: 'repeat(2, 1fr)',
+// 					sm: 'repeat(1, 1fr)',
+// 				}}
+// 				gap={6}
+// 				mt={'22px'}
+// 			>
+// 				<GridItem>
+// 					<Card
+// 						onClick={() => null}
+// 						opacity={1}
+// 						shadow="md"
+// 						_hover={{ shadow: 'xl' }}
+// 						border="1px solid #EDF2F7;"
+// 						borderRadius="lg"
+// 					>
+// 						<CardBody minH={'412px'}>
+// 							<Stack justifyContent={'space-between'}>
+// 								<Stack>
+// 									<Stack direction="row" align={'center'} justify={'space-between'}>
+// 										<Stack direction="row" align={'center'}>
+// 											<Image src={OxImg} width={24} height={24} alt="socialPng" />
+// 											<Heading onClick={e => null} size="md" color="gray.800">
+// 												OX Points
+// 											</Heading>
+// 										</Stack>
+// 										{isDesktop && (
+// 											<Stack>
+// 												<Text color={'pink.500'} fontSize={'md'}>
+// 													Updated at 8:00 AM daily (GMT+8)
+// 												</Text>
+// 											</Stack>
+// 										)}
+// 									</Stack>
+// 									{!isDesktop && (
+// 										<Stack>
+// 											<Text color={'pink.500'} fontSize={'md'}>
+// 												Updated at 8:00 AM daily (GMT+8)
+// 											</Text>
+// 										</Stack>
+// 									)}
+// 									<Stack mt={'26px'}>
+// 										<Text color={'#1A202C'} fontSize={'md'}>
+// 											Your Balance
+// 										</Text>
+// 									</Stack>
+// 									<Stack align={'end'} direction={'row'}>
+// 										<Text color={'#1A202C'} fontSize={'4xl'}>
+// 											{userPointData?.balance}
+// 										</Text>
+// 										<Text ml={'12px'} mb={'8px'} color={'#1A202C'} fontSize={'md'}>
+// 											OX Points
+// 										</Text>
+// 									</Stack>
+// 								</Stack>
+// 								<Stack>
+// 									<Stack mt={'50px'}>
+// 										<Stack>
+// 											<Text color={'#1A202C'} fontSize={'md'}>
+// 												How to Earn OX Points?
+// 											</Text>
+// 										</Stack>
+// 										<Stack direction={'row'}>
+// 											<Stack w={'6px'} h={'6px'} mt={2} borderRadius={'3px'} bg={'#1A202C'} />
+// 											<Text color="#1A202C" size="sm">
+// 												The platform will send OX Points to you based on the proportion of daily
+// 												transactions.
+// 											</Text>
+// 										</Stack>
+// 										<Stack direction={'row'}>
+// 											<Stack w={'6px'} h={'6px'} mt={2} borderRadius={'3px'} bg={'#1A202C'} />
+// 											<Text color="#1A202C" size="sm">
+// 												By referring friends, you can earn OX points. Learn more about the{' '}
+// 												<Text
+// 													as="span"
+// 													onClick={() => router.push('./referral')}
+// 													cursor="pointer"
+// 													color="#4299E1"
+// 													size="sm"
+// 												>
+// 													referral programs.
+// 												</Text>
+// 											</Text>
+// 										</Stack>
+// 									</Stack>
+// 								</Stack>
+// 							</Stack>
+// 						</CardBody>
+// 					</Card>
+// 				</GridItem>
+// 				<GridItem>
+// 					<Card
+// 						flex={1}
+// 						onClick={() => null}
+// 						opacity={1}
+// 						shadow="md"
+// 						_hover={{ shadow: 'xl' }}
+// 						border="1px solid #EDF2F7;"
+// 						borderRadius="lg"
+// 					>
+// 						<CardBody minH={'412px'}>
+// 							<Stack direction="row">
+// 								<Stack>
+// 									<Stack direction={'row'}>
+// 										<Image src={RedeemImg} width={24} height={24} alt="socialPng" />
+// 										<Heading onClick={e => null} size="md" color="gray.800">
+// 											Redemption Events
+// 										</Heading>
+// 									</Stack>
+// 								</Stack>
+// 							</Stack>
+// 							<Stack h="112px" mt={'40px'} mb={'40px'} align={'center'}>
+// 								<Image src={ExchangeImg} width={155} alt="socialPng" />
+// 							</Stack>
+// 							<Stack>
+// 								<Text textAlign={'center'}>
+// 									Use OX Points to redeem rewards such as USDT, airdrops, and popular products.
+// 								</Text>
+// 							</Stack>
+// 							<Stack mt={'36px'}>
+// 								<Button
+// 									onClick={() => router.push('./events')}
+// 									// w={{ lg: '172px', md: '100px', sm: '100px' }}
+// 									h={{ lg: '48px', md: '40px', sm: '40px' }}
+// 									colorScheme="teal"
+// 								>
+// 									View Events
+// 								</Button>
+// 							</Stack>
+// 						</CardBody>
+// 					</Card>
+// 				</GridItem>
+// 			</Grid>
+// 			<Card
+// 				flex={1}
+// 				mt={8}
+// 				ml={paddingMainHorizontal}
+// 				mr={paddingMainHorizontal}
+// 				onClick={() => null}
+// 				opacity={1}
+// 				shadow="md"
+// 				_hover={{ shadow: 'xl' }}
+// 				border="1px solid #EDF2F7;"
+// 				borderRadius="lg"
+// 			>
+// 				<CardBody minH={'312px'}>
+// 					<Stack direction="row">
+// 						<Stack>
+// 							<Stack direction={'row'}>
+// 								<Image src={PointsHistoryImg} width={24} height={24} alt="socialPng" />
+// 								<Heading onClick={e => null} size="md" color="gray.800">
+// 									Points History
+// 								</Heading>
+// 							</Stack>
+// 						</Stack>
+// 					</Stack>
+// 					{/* <Stack h="112px" mt={'40px'} mb={'40px'} align={'center'}>
+// 						<Image src={ExchangeImg} width={155} alt="socialPng" />
+// 					</Stack> */}
+// 					<Stack>
+// 						<Text textAlign={'end'}>
+// 							Lifetime Earned:{' '}
+// 							<Text as="span" color="#1A202C" size="sm">
+// 								{`${userPointData?.earned?.toLocaleString()} OX Points`}
+// 							</Text>
+// 						</Text>
+// 						<Text textAlign={'end'}>
+// 							Total Redeemed:{' '}
+// 							<Text as="span" color="#1A202C" size="sm">
+// 								{`${userPointData?.claimed?.toLocaleString()} OX Points`}
+// 							</Text>
+// 						</Text>
+// 					</Stack>
+// 					{meta?.page === 1 && userPointData?.points.length === 0 ? null : (
+// 						<Stack>
+// 							<Tabs mt={'28px'} index={0} onChange={handleTabsChange}>
+// 								<TabPanels>
+// 									<TabPanel p={0}>
+// 										<PointsHistoryTable pageCount={meta.pageCount} />
+// 									</TabPanel>
+// 								</TabPanels>
+// 							</Tabs>
+// 						</Stack>
+// 					)}
+// 				</CardBody>
+// 			</Card>
+// 			<Card
+// 				flex={1}
+// 				mt={8}
+// 				ml={paddingMainHorizontal}
+// 				mr={paddingMainHorizontal}
+// 				onClick={() => null}
+// 				opacity={1}
+// 				shadow="md"
+// 				_hover={{ shadow: 'xl' }}
+// 				border="1px solid #EDF2F7;"
+// 				borderRadius="lg"
+// 			></Card>
+// 		</Stack>
+// 	);
+// }
+
+// export default OxPoints;
+
 import React, { useEffect } from 'react';
 import {
 	Stack,
@@ -133,7 +444,7 @@ function OxPoints() {
 										</Stack>
 										{isDesktop && (
 											<Stack>
-												<Text color={'pink.500'} fontSize={'md'}>
+												<Text color={'#1A202C'} fontSize={'md'}>
 													Updated at 8:00 AM daily (GMT+8)
 												</Text>
 											</Stack>
@@ -141,14 +452,14 @@ function OxPoints() {
 									</Stack>
 									{!isDesktop && (
 										<Stack>
-											<Text color={'pink.500'} fontSize={'md'}>
+											<Text color={'#1A202C'} fontSize={'md'}>
 												Updated at 8:00 AM daily (GMT+8)
 											</Text>
 										</Stack>
 									)}
 									<Stack mt={'26px'}>
 										<Text color={'#1A202C'} fontSize={'md'}>
-											Your Balance
+											Available Balance
 										</Text>
 									</Stack>
 									<Stack align={'end'} direction={'row'}>
@@ -261,33 +572,38 @@ function OxPoints() {
 							</Stack>
 						</Stack>
 					</Stack>
-					<Stack h="112px" mt={'40px'} mb={'40px'} align={'center'}>
-						<Image src={ExchangeImg} width={155} alt="socialPng" />
-					</Stack>
-					<Stack>
-						<Text textAlign={'end'}>
-							Lifetime Earned:{' '}
-							<Text as="span" color="#1A202C" size="sm">
-								{`${userPointData?.earned?.toLocaleString()} OX Points`}
+					{meta?.page === 1 && userPointData?.points.length === 0 ? (
+						<Stack h={'100%'} justifyContent={'center'} align={'center'}>
+							<Text color={'#1A202C'} fontSize={'md'}>
+								There is no data availavle
 							</Text>
-						</Text>
-						<Text textAlign={'end'}>
-							Total Redeemed:{' '}
-							<Text as="span" color="#1A202C" size="sm">
-								{`${userPointData?.claimed?.toLocaleString()} OX Points`}
-							</Text>
-						</Text>
-					</Stack>
-					{meta?.page === 1 && userPointData?.points.length === 0 ? null : (
-						<Stack>
-							<Tabs mt={'28px'} index={0} onChange={handleTabsChange}>
-								<TabPanels>
-									<TabPanel p={0}>
-										<PointsHistoryTable pageCount={meta.pageCount} />
-									</TabPanel>
-								</TabPanels>
-							</Tabs>
 						</Stack>
+					) : (
+						<>
+							<Stack>
+								<Text textAlign={'end'}>
+									Lifetime Earned:{' '}
+									<Text as="span" color="#1A202C" size="sm">
+										{`${userPointData?.earned?.toLocaleString()} OX Points`}
+									</Text>
+								</Text>
+								<Text textAlign={'end'}>
+									Total Redeemed:{' '}
+									<Text as="span" color="#1A202C" size="sm">
+										{`${userPointData?.claimed?.toLocaleString()} OX Points`}
+									</Text>
+								</Text>
+							</Stack>
+							<Stack>
+								<Tabs mt={'28px'} index={0} onChange={handleTabsChange}>
+									<TabPanels>
+										<TabPanel p={0}>
+											<PointsHistoryTable pageCount={meta.pageCount} />
+										</TabPanel>
+									</TabPanels>
+								</Tabs>
+							</Stack>
+						</>
 					)}
 				</CardBody>
 			</Card>
